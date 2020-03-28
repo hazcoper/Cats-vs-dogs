800f7a9682b0520d463346d667cfed0aed1c1c7d
Hazcoper <zemariamateus@gmail.com>
2020-03-28 22:49:30 +0000

    Added noise to the picture

--- Scratch.py
+++ Scratch.py
@@ -1,12 +1,28 @@
-from PIL import Image
 import os
-print("heklo")
-exit()
-print(os.getcwd())
-def flip_image(img, number):
-    """Receives the number of the image and the new number to save the flipped image"""
-    img = Image.open(img)
-    img.transpose(Image.FLIP_LEFT_RIGHT).save(cats + "/" + str(number)+ ".jpg")
+from skimage.util import random_noise
+import cv2
+import numpy as np
+
+
+
+def add_noise(img, number):
+    """Receives an image adds noise flips and saves as a new image with the right number"""
+    # Load the image
+    img = cv2.imread(img)
+
+    #Flip Image
+    img = cv2.flip(img, 1)
+
+    # Add salt-and-pepper noise to the image.
+    noise_img = random_noise(img, mode='s&p',amount=0.3)
+    
+    # The above function returns a floating-point image
+    # on the range [0, 1], thus we changed it to 'uint8'
+    # and from [0,255]
+    noise_img = np.array(255*noise_img, dtype = 'uint8')
+    
+    #Save image
+    cv2.imwrite(cats + "/" + str(number) + ".jpg", noise_img)
 
 cats = "Dog"
 total = len(os.listdir(cats))
@@ -14,6 +30,7 @@ counter = 12499
 for cat in range(total):
     counter = counter + 1
     try:
-        flip_image(cats + "/" + str(cat)+".jpg", counter)
+        add_noise(cats + "/" + str(cat)+".jpg", counter)
+        break
     except:
         print("image failed: ", cat)
